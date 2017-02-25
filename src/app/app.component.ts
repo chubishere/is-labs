80477852b8fd50905dcc72b7759a0fc78a022ada
chubishere <clarence@dubishere.com>
2017-02-25 12:02:24 +0000

    add dynamic project data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
 import '../sass/main.scss';
-
-import {Project, projects} from './data-projects';
+import {Project} from './data-projects';
 
 @Component({
   selector: 'my-app',
   template: `
-		<h1>Hello {{name}}</h1>
-		<div>
-			<h2>Projects</h2>
-			<ul class="projects">
-				<li *ngFor="let p of projects">Project {{p}}
-					<div>{{p.client}}</div>
-				</li>
-			</ul>
+	<h1>Hello {{name}}</h1>
+	<div>
+		<h2>Projects</h2>
+		<div class="projects-list">
+			<div class="projects-list__item">
+				<div class="projects-list__field client">Client</div>
+				<div class="projects-list__field client">Project</div>
+				<div class="projects-list__field start-date">Start date</div>
+				<div class="projects-list__field end-date">End date</div>
+				<div class="projects-list__field stack">Stack</div>
+				<div class="projects-list__field ssl-expiration">SSL expiration</div>
+			</div>
 		</div>
+		<ul class="projects-list list-unstyled">
+			<li class="projects-list__item"
+				*ngFor="let p of projects; let odd = odd; let even = even"
+				[ngClass]="{even:even, odd:odd}"
+			>
+				<div class="projects-list__field client">{{p.client}}</div>
+				<div class="projects-list__field project">{{p.project}}</div>
+				<div class="projects-list__field start-date">{{p.start_date | date:'short'}}</div>
+				<div class="projects-list__field end-date">{{p.end_date | date:'short'}}</div>
+				<div class="projects-list__field stack">{{p.stack}}</div>
+				<div class="projects-list__field ssl-expiration">{{p.ssl_expiration}}</div>
+			</li>
+		</ul>
+	</div>
 	`
 })
 export class AppComponent { 
 	name:String = 'Roberto';
-	projects:Array<Project> = projects;
+	projects:Array<Project> = Project.load();
 }
--- src/app/data-projects.ts
+++ src/app/data-projects.ts
@@ -1,108 +1,68 @@
-export interface ProjectData {
-    id:number,
-    client:string,
-    start_date:string,
-    end_date:string,
-    stack:string,
-    ssl_expiration:string
-};
-export const projects:Array<Project> = [{"id":1,"client":"emulation","start_date":"2016-02-26T06:40:48Z","end_date":"2016-09-27T09:06:30Z","stack":"CN","ssl_expiration":"2019-10-21T13:26:46Z"},
-{"id":2,"client":"intermediate","start_date":"2016-05-23T23:51:17Z","end_date":"2017-01-24T11:36:27Z","stack":"CN","ssl_expiration":"2016-07-28T20:22:34Z"},
-{"id":3,"client":"Proactive","start_date":"2017-01-24T02:08:00Z","end_date":"2016-07-02T01:13:13Z","stack":"CN","ssl_expiration":null},
-{"id":4,"client":"time-frame","start_date":"2016-05-13T11:43:31Z","end_date":"2016-02-03T23:48:28Z","stack":"CN","ssl_expiration":null},
-{"id":5,"client":"Stand-alone","start_date":"2016-07-24T10:13:51Z","end_date":"2016-09-05T03:26:03Z","stack":"CN","ssl_expiration":null},
-{"id":6,"client":"high-level","start_date":"2016-10-04T20:05:55Z","end_date":"2016-12-13T23:53:32Z","stack":"CN","ssl_expiration":null},
-{"id":7,"client":"Open-source","start_date":"2016-11-03T07:32:36Z","end_date":"2017-02-03T13:12:40Z","stack":"CN","ssl_expiration":null},
-{"id":8,"client":"bottom-line","start_date":"2016-08-02T02:17:46Z","end_date":"2016-03-10T02:57:15Z","stack":"CN","ssl_expiration":"2019-08-21T08:20:55Z"},
-{"id":9,"client":"systemic","start_date":"2016-06-14T07:08:01Z","end_date":"2016-03-14T18:03:32Z","stack":"CN","ssl_expiration":"2016-04-24T07:55:35Z"},
-{"id":10,"client":"mission-critical","start_date":"2016-11-10T02:11:11Z","end_date":"2016-05-08T00:44:46Z","stack":"CN","ssl_expiration":null},
-{"id":11,"client":"full-range","start_date":"2016-12-06T03:26:27Z","end_date":"2016-05-29T19:03:53Z","stack":"CN","ssl_expiration":null},
-{"id":12,"client":"Triple-buffered","start_date":"2016-06-23T04:21:27Z","end_date":"2016-09-29T14:29:17Z","stack":"CN","ssl_expiration":null},
-{"id":13,"client":"architecture","start_date":"2016-03-05T07:16:28Z","end_date":"2016-11-05T12:06:30Z","stack":"CN","ssl_expiration":"2016-11-07T21:41:05Z"},
-{"id":14,"client":"secured line","start_date":"2016-07-14T21:13:36Z","end_date":"2016-06-25T10:42:09Z","stack":"US","ssl_expiration":null},
-{"id":15,"client":"conglomeration","start_date":"2016-08-15T18:17:47Z","end_date":"2016-04-21T11:17:10Z","stack":"CN","ssl_expiration":null},
-{"id":16,"client":"conglomeration","start_date":"2016-11-20T12:05:54Z","end_date":"2016-08-30T12:39:25Z","stack":"CN","ssl_expiration":null},
-{"id":17,"client":"Re-contextualized","start_date":"2016-01-10T00:04:05Z","end_date":"2016-02-27T01:29:32Z","stack":"CN","ssl_expiration":null},
-{"id":18,"client":"flexibility","start_date":"2016-03-03T21:35:02Z","end_date":"2016-08-20T12:57:27Z","stack":"US","ssl_expiration":null},
-{"id":19,"client":"Adaptive","start_date":"2016-04-05T18:13:37Z","end_date":"2016-11-11T02:28:11Z","stack":"CN","ssl_expiration":null},
-{"id":20,"client":"product","start_date":"2016-08-13T05:23:23Z","end_date":"2016-10-18T20:57:23Z","stack":"CN","ssl_expiration":null},
-{"id":21,"client":"core","start_date":"2016-06-09T19:20:49Z","end_date":"2016-02-24T15:57:40Z","stack":"CN","ssl_expiration":null},
-{"id":22,"client":"intranet","start_date":"2016-08-19T10:36:36Z","end_date":"2016-06-01T08:23:48Z","stack":"CN","ssl_expiration":null},
-{"id":23,"client":"paradigm","start_date":"2016-07-25T04:07:18Z","end_date":"2016-05-05T11:40:33Z","stack":"CN","ssl_expiration":"2017-09-24T21:19:53Z"},
-{"id":24,"client":"leverage","start_date":"2016-02-06T04:53:28Z","end_date":"2016-03-31T02:38:31Z","stack":"CN","ssl_expiration":null},
-{"id":25,"client":"Re-engineered","start_date":"2016-10-11T07:56:31Z","end_date":"2017-02-16T04:56:19Z","stack":"CN","ssl_expiration":null},
-{"id":26,"client":"methodology","start_date":"2016-07-03T12:28:20Z","end_date":"2016-08-06T16:00:48Z","stack":"CN","ssl_expiration":"2019-01-03T09:51:53Z"},
-{"id":27,"client":"responsive","start_date":"2016-07-17T13:25:56Z","end_date":"2017-02-16T23:03:52Z","stack":"CN","ssl_expiration":null},
-{"id":28,"client":"core","start_date":"2016-04-30T08:49:22Z","end_date":"2017-01-24T07:07:51Z","stack":"CN","ssl_expiration":"2017-12-05T10:31:20Z"},
-{"id":29,"client":"6th generation","start_date":"2017-01-15T19:56:21Z","end_date":"2016-05-07T07:02:15Z","stack":"CN","ssl_expiration":"2018-10-07T00:58:25Z"},
-{"id":30,"client":"array","start_date":"2016-03-18T23:35:24Z","end_date":"2016-08-11T09:58:36Z","stack":"CN","ssl_expiration":null},
-{"id":31,"client":"Progressive","start_date":"2016-10-14T02:55:50Z","end_date":"2017-02-05T01:50:36Z","stack":"CN","ssl_expiration":"2016-05-19T11:58:04Z"},
-{"id":32,"client":"coherent","start_date":"2016-11-13T07:50:26Z","end_date":"2016-09-17T17:49:34Z","stack":"CN","ssl_expiration":"2020-03-09T13:34:36Z"},
-{"id":33,"client":"Managed","start_date":"2016-12-19T04:35:42Z","end_date":"2016-02-24T19:22:50Z","stack":"CN","ssl_expiration":"2018-10-12T15:59:55Z"},
-{"id":34,"client":"background","start_date":"2016-10-03T03:44:02Z","end_date":"2017-01-14T04:51:05Z","stack":"IE","ssl_expiration":null},
-{"id":35,"client":"contextually-based","start_date":"2016-02-19T19:20:19Z","end_date":"2016-02-22T12:57:31Z","stack":"CN","ssl_expiration":null},
-{"id":36,"client":"Object-based","start_date":"2016-09-14T17:12:18Z","end_date":"2016-04-13T18:24:01Z","stack":"US","ssl_expiration":null},
-{"id":37,"client":"database","start_date":"2016-12-06T17:09:13Z","end_date":"2016-05-09T08:06:04Z","stack":"CN","ssl_expiration":null},
-{"id":38,"client":"Exclusive","start_date":"2016-11-05T04:53:19Z","end_date":"2016-12-13T10:21:32Z","stack":"CN","ssl_expiration":null},
-{"id":39,"client":"4th generation","start_date":"2016-11-26T10:45:51Z","end_date":"2017-02-01T18:37:12Z","stack":"US","ssl_expiration":"2019-12-22T21:19:29Z"},
-{"id":40,"client":"even-keeled","start_date":"2016-04-04T02:19:34Z","end_date":"2016-06-14T14:09:41Z","stack":"CN","ssl_expiration":"2018-04-08T00:23:11Z"},
-{"id":41,"client":"Assimilated","start_date":"2016-11-21T21:18:45Z","end_date":"2016-11-10T15:57:31Z","stack":"CN","ssl_expiration":"2019-05-23T06:16:29Z"},
-{"id":42,"client":"Cross-group","start_date":"2016-01-09T02:09:39Z","end_date":"2016-12-24T13:31:11Z","stack":"CN","ssl_expiration":null},
-{"id":43,"client":"Synergistic","start_date":"2016-12-24T12:05:55Z","end_date":"2016-12-31T18:23:56Z","stack":"US","ssl_expiration":null},
-{"id":44,"client":"optimal","start_date":"2016-05-16T23:24:33Z","end_date":"2016-10-22T23:41:20Z","stack":"CN","ssl_expiration":null},
-{"id":45,"client":"Universal","start_date":"2017-01-01T18:17:19Z","end_date":"2016-05-28T11:04:02Z","stack":"CN","ssl_expiration":null},
-{"id":46,"client":"executive","start_date":"2017-01-20T08:04:18Z","end_date":"2016-03-05T13:56:39Z","stack":"CN","ssl_expiration":null},
-{"id":47,"client":"pricing structure","start_date":"2016-09-11T11:05:04Z","end_date":"2016-11-04T03:39:20Z","stack":"CN","ssl_expiration":null},
-{"id":48,"client":"paradigm","start_date":"2016-05-29T23:21:56Z","end_date":"2016-05-15T20:18:50Z","stack":"CN","ssl_expiration":"2017-08-03T01:01:48Z"},
-{"id":49,"client":"Multi-layered","start_date":"2016-08-15T21:22:35Z","end_date":"2016-07-03T17:43:38Z","stack":"CN","ssl_expiration":null},
-{"id":50,"client":"Assimilated","start_date":"2016-04-02T12:11:06Z","end_date":"2016-02-27T10:50:54Z","stack":"CN","ssl_expiration":null},
-{"id":51,"client":"hub","start_date":"2016-07-07T10:53:42Z","end_date":"2016-10-27T00:41:50Z","stack":"CN","ssl_expiration":"2017-07-21T13:50:25Z"},
-{"id":52,"client":"empowering","start_date":"2016-12-16T02:07:37Z","end_date":"2016-05-19T00:48:25Z","stack":"CN","ssl_expiration":null},
-{"id":53,"client":"challenge","start_date":"2016-02-15T17:19:25Z","end_date":"2016-05-19T06:14:58Z","stack":"CN","ssl_expiration":"2017-05-08T00:42:09Z"},
-{"id":54,"client":"conglomeration","start_date":"2016-09-13T16:00:06Z","end_date":"2016-04-17T20:52:18Z","stack":"US","ssl_expiration":null},
-{"id":55,"client":"User-friendly","start_date":"2016-03-11T00:37:06Z","end_date":"2016-08-09T19:23:38Z","stack":"CN","ssl_expiration":null},
-{"id":56,"client":"foreground","start_date":"2016-09-23T05:39:31Z","end_date":"2016-03-27T10:46:26Z","stack":"CN","ssl_expiration":null},
-{"id":57,"client":"static","start_date":"2016-09-28T10:27:36Z","end_date":"2016-05-14T00:37:52Z","stack":"CN","ssl_expiration":"2019-11-18T06:52:20Z"},
-{"id":58,"client":"system-worthy","start_date":"2016-01-15T18:16:34Z","end_date":"2016-09-03T01:57:19Z","stack":"CN","ssl_expiration":null},
-{"id":59,"client":"Digitized","start_date":"2016-12-07T05:45:38Z","end_date":"2016-05-15T13:56:20Z","stack":"CN","ssl_expiration":null},
-{"id":60,"client":"demand-driven","start_date":"2016-04-29T10:09:49Z","end_date":"2016-04-03T07:05:57Z","stack":"CN","ssl_expiration":"2019-02-23T12:56:50Z"},
-{"id":61,"client":"adapter","start_date":"2016-03-10T11:45:20Z","end_date":"2016-06-09T14:42:04Z","stack":"CN","ssl_expiration":"2017-03-29T13:59:16Z"},
-{"id":62,"client":"Face to face","start_date":"2016-03-17T20:31:17Z","end_date":"2016-02-14T00:28:04Z","stack":"CN","ssl_expiration":"2018-11-14T05:00:09Z"},
-{"id":63,"client":"Exclusive","start_date":"2016-08-21T14:55:13Z","end_date":"2017-01-24T05:18:46Z","stack":"US","ssl_expiration":null},
-{"id":64,"client":"Synergized","start_date":"2016-12-07T22:47:41Z","end_date":"2016-06-04T04:23:47Z","stack":"CN","ssl_expiration":null},
-{"id":65,"client":"directional","start_date":"2016-10-14T15:08:35Z","end_date":"2017-01-27T19:57:02Z","stack":"CN","ssl_expiration":null},
-{"id":66,"client":"open architecture","start_date":"2016-02-24T22:10:43Z","end_date":"2016-12-29T03:54:39Z","stack":"CN","ssl_expiration":"2018-10-31T19:22:42Z"},
-{"id":67,"client":"Grass-roots","start_date":"2016-04-21T00:11:52Z","end_date":"2016-07-13T21:37:23Z","stack":"CN","ssl_expiration":null},
-{"id":68,"client":"background","start_date":"2016-12-30T20:56:38Z","end_date":"2016-09-05T14:37:36Z","stack":"CN","ssl_expiration":"2018-12-19T05:38:41Z"},
-{"id":69,"client":"tangible","start_date":"2016-12-25T12:13:45Z","end_date":"2016-11-26T15:14:20Z","stack":"CN","ssl_expiration":null},
-{"id":70,"client":"Stand-alone","start_date":"2016-05-05T06:31:35Z","end_date":"2016-04-02T18:34:25Z","stack":"CN","ssl_expiration":null},
-{"id":71,"client":"project","start_date":"2016-05-20T10:44:37Z","end_date":"2016-11-20T08:54:12Z","stack":"CN","ssl_expiration":"2018-08-21T15:37:16Z"},
-{"id":72,"client":"info-mediaries","start_date":"2016-10-02T22:53:23Z","end_date":"2016-06-19T06:45:49Z","stack":"CN","ssl_expiration":"2019-06-05T03:22:54Z"},
-{"id":73,"client":"Managed","start_date":"2016-06-17T19:21:29Z","end_date":"2016-05-07T08:29:56Z","stack":"CN","ssl_expiration":"2018-05-08T12:34:34Z"},
-{"id":74,"client":"global","start_date":"2016-08-23T11:21:50Z","end_date":"2016-05-30T07:12:12Z","stack":"CN","ssl_expiration":null},
-{"id":75,"client":"Stand-alone","start_date":"2017-01-02T10:19:52Z","end_date":"2016-07-14T09:57:00Z","stack":"CN","ssl_expiration":null},
-{"id":76,"client":"Organized","start_date":"2016-03-15T09:20:58Z","end_date":"2016-04-19T03:59:29Z","stack":"CN","ssl_expiration":"2019-10-11T22:17:49Z"},
-{"id":77,"client":"Innovative","start_date":"2016-10-23T18:54:20Z","end_date":"2016-10-16T12:48:46Z","stack":"CN","ssl_expiration":null},
-{"id":78,"client":"explicit","start_date":"2017-01-25T07:03:32Z","end_date":"2016-11-07T02:57:30Z","stack":"US","ssl_expiration":null},
-{"id":79,"client":"impactful","start_date":"2016-09-23T09:30:03Z","end_date":"2016-10-10T00:35:09Z","stack":"CN","ssl_expiration":"2019-08-15T21:12:09Z"},
-{"id":80,"client":"clear-thinking","start_date":"2016-01-25T10:23:21Z","end_date":"2016-09-27T00:27:58Z","stack":"CN","ssl_expiration":null},
-{"id":81,"client":"policy","start_date":"2016-12-18T00:23:34Z","end_date":"2016-12-07T13:10:59Z","stack":"CN","ssl_expiration":null},
-{"id":82,"client":"Compatible","start_date":"2016-03-23T20:16:51Z","end_date":"2016-06-03T14:20:14Z","stack":"CN","ssl_expiration":"2018-09-13T16:57:38Z"},
-{"id":83,"client":"encompassing","start_date":"2017-01-13T03:54:37Z","end_date":"2016-12-02T13:33:03Z","stack":"CN","ssl_expiration":null},
-{"id":84,"client":"help-desk","start_date":"2016-12-02T06:44:46Z","end_date":"2016-06-22T23:45:39Z","stack":"CN","ssl_expiration":"2019-11-30T22:34:47Z"},
-{"id":85,"client":"Vision-oriented","start_date":"2016-09-22T16:28:58Z","end_date":"2017-01-19T22:56:19Z","stack":"CN","ssl_expiration":null},
-{"id":86,"client":"transitional","start_date":"2016-06-07T15:37:18Z","end_date":"2016-04-02T06:39:30Z","stack":"CN","ssl_expiration":null},
-{"id":87,"client":"tangible","start_date":"2016-09-24T21:26:10Z","end_date":"2016-11-26T12:21:31Z","stack":"CN","ssl_expiration":null},
-{"id":88,"client":"multi-tasking","start_date":"2016-11-25T19:13:20Z","end_date":"2016-11-24T14:38:48Z","stack":"CN","ssl_expiration":null},
-{"id":89,"client":"benchmark","start_date":"2016-10-13T03:52:16Z","end_date":"2016-08-09T16:44:34Z","stack":"CN","ssl_expiration":null},
-{"id":90,"client":"time-frame","start_date":"2016-08-28T16:50:44Z","end_date":"2016-08-21T21:57:53Z","stack":"CN","ssl_expiration":"2019-07-22T12:42:23Z"},
-{"id":91,"client":"software","start_date":"2016-07-09T01:16:20Z","end_date":"2016-04-27T16:29:40Z","stack":"CN","ssl_expiration":null},
-{"id":92,"client":"challenge","start_date":"2016-06-06T08:22:14Z","end_date":"2016-02-17T18:23:53Z","stack":"CN","ssl_expiration":null},
-{"id":93,"client":"user-facing","start_date":"2016-03-27T20:08:56Z","end_date":"2016-07-26T13:57:50Z","stack":"CN","ssl_expiration":null},
-{"id":94,"client":"Polarised","start_date":"2016-04-20T09:30:11Z","end_date":"2017-02-19T11:58:30Z","stack":"CN","ssl_expiration":"2020-05-08T03:42:43Z"},
-{"id":95,"client":"local area network","start_date":"2016-03-07T09:04:10Z","end_date":"2016-04-13T03:10:17Z","stack":"CN","ssl_expiration":null},
-{"id":96,"client":"flexibility","start_date":"2016-11-27T19:26:40Z","end_date":"2016-12-09T10:15:04Z","stack":"CN","ssl_expiration":null},
-{"id":97,"client":"toolset","start_date":"2016-08-04T16:30:01Z","end_date":"2017-01-03T09:46:02Z","stack":"CN","ssl_expiration":null},
-{"id":98,"client":"User-friendly","start_date":"2016-11-01T14:03:06Z","end_date":"2016-06-19T01:14:47Z","stack":"US","ssl_expiration":null},
-{"id":99,"client":"ability","start_date":"2016-09-09T15:21:11Z","end_date":"2016-05-18T05:49:04Z","stack":"CN","ssl_expiration":null},
-{"id":100,"client":"circuit","start_date":"2016-07-06T21:07:15Z","end_date":"2016-11-20T01:02:14Z","stack":"CN","ssl_expiration":null}];
+import * as moment from 'moment';
+
+export class Project {
+    
+    //
+    // ─── STATIC METHODS ─────────────────────────────────────────────────────────────
+    //
+        
+    static load():Array<Project> {
+        let out:Array<Project> = [];
+        let i = 100;
+        while (i--) {
+           out.push(new Project); 
+        }
+        return out;
+    }
+
+    //
+    // ─── INSTANCE VARS ──────────────────────────────────────────────────────────────
+    //
+
+    client:string;
+    project:string;
+    start_date:string;
+    end_date:string;
+    end_date_admin:string;
+    stack:string;
+    ssl_expiration:string;
+
+    constructor() {
+
+        //
+        // ─── CLIENT ────────────────────────────────────────────────
+        //
+        this.client = this.randomFrom(['Insites', 'MMR', 'FaceGroup', 'Behavioural Architects', 'Verve']);
+
+        //
+        // ─── PROJECT NAME ────────────────────────────────────────────────
+        //
+        this.project = 
+            this.randomFrom(['Dog', 'Face', 'Leg', 'Elderly', 'Train', 'Driver', 'Idol', 'Animal', 'Food'])
+            + this.randomFrom(['Project', 'Diaries', 'Community', 'Care', 'Appreciation', 'Lovers Anonymous', 'Peevs', 'Spotters Club', 'Afficiandos Survey']);
+
+        //
+        // ─── DATES ──────────────────────────────────────────────────
+        //
+        let start           = moment().add((Math.floor(Math.random()*100) - 50), 'days');
+        let end             = start.add(Math.ceil(Math.random()*100), 'days');
+        let end_admin       = end.add(6, 'months');
+        let ssl_expiration  = end.add(Math.ceil(Math.random()*100), 'days');
+        this.start_date     = start.format();
+        this.end_date       = end.format();
+        this.end_date_admin = end_admin.format();
+        if (Math.random() < 0.2) {
+            this.ssl_expiration = end_admin.format();
+        }
+
+        //
+        // ─── STACK ───────────────────────────────────────────────────────
+        //
+        this.stack = this.randomFrom(['ASIA', 'EU', 'US']);
+
+    }
+
+    private randomFrom(items:Array<any>):any {
+        return items[Math.floor(Math.random() * items.length)];
+    }
+}
